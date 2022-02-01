a306af1b11dce69c10f778f2bd281441d9124c9f
beddeb <DZarankovich@yandex.ru>
2022-02-01 18:33:29 +0300

    PySorts

--- DEFrockSortNewArr.py
+++ DEFrockSortNewArr.py
@@ -1,24 +1,8 @@
-import random
-
-def selectionSort(temp):
-    cout = 0
-    for x in range(len(temp) - 1):
-        nMin = x
-        for y in range(x + 1, len(temp)):
-            if temp[y] < temp[nMin]: nMin = y
-        if x != nMin: temp[x], temp[nMin] = temp[nMin], temp[x]; cout += 1
-    return temp, cout
-
-def bubbleSort(temp):
-    cout = 0
-    for x in range(len(temp) - 1):
-        for y in range(len(temp) - 2, x - 1, -1):
-            if temp[y + 1] < temp[y]: temp[y], temp[y + 1] = temp[y + 1], temp[y]; cout += 1
-    return temp, cout
-
-f1 = [random.randint(1, 10000) for _ in range(1000)]
-f2 = f1.copy()
-print(f'Base() ArrLen: {len(f1)}')
-print(f'selectionSort() ArrChanges: {selectionSort(f1)[1]}')
-print(f'bubbleSort() ArrChanges: {bubbleSort(f2)[1]}')
-
+def rockSort(temp):
+    x = 1
+    while x < len(temp):
+        for i in range(len(temp) - x):
+            if temp[i] > temp[i + 1]:
+                temp[i], temp[i + 1] = temp[i + 1], temp[i]
+        x += 1
+    return temp
