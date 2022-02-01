f4f4f3462110a5e188a19975dad75313065dad81
beddeb <DZarankovich@yandex.ru>
2022-02-01 16:48:34 +0300

    Update qSortNewArr.py
    qSortNewArr

--- qSortNewArr.py
+++ qSortNewArr.py
@@ -5,5 +5,5 @@ def quicksort(temp):
     else: qwerty = random.choice(temp)
     x_arr = [i for i in temp if i < qwerty]
     y_arr = [qwerty] * temp.count(qwerty)
-    z_arr = [n for n in temp if n > qwerty]
+    z_arr = [i for i in temp if i > qwerty]
     return quicksort(x_arr) + y_arr + quicksort(z_arr)
