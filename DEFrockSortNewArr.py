1537bb98119b4c51cd3d10fa16f4e5b016bdb4a8
beddeb <DZarankovich@yandex.ru>
2022-02-01 18:27:48 +0300

    Delete DEFrockSortNewArr.py

--- DEFrockSortNewArr.py
+++
@@ -1,8 +0,0 @@
-def rockSort(temp):
-    x = 1
-    while x < len(temp):
-        for i in range(len(temp) - x):
-            if temp[i] > temp[i + 1]:
-                temp[i], temp[i + 1] = temp[i + 1], temp[i]
-        x += 1
-    return temp
