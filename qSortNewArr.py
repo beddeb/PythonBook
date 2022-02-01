aea25c75b1ed401e8a16dcc745331a362a01e630
beddeb <DZarankovich@yandex.ru>
2022-02-01 16:57:43 +0300

    Delete qSortNewArr.py

--- qSortNewArr.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def quicksort(temp):
-    if len(temp) <= 1: return temp
-    else: qwerty = random.choice(temp)
-    x_arr = [i for i in temp if i < qwerty]
-    y_arr = [qwerty] * temp.count(qwerty)
-    z_arr = [i for i in temp if i > qwerty]
-    return quicksort(x_arr) + y_arr + quicksort(z_arr)
